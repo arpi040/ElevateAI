f236a26bdb9c2e669320fce90ff9f39d402c3e3f
Arpita <arpitaver040@gmail.com>
2026-07-29 15:22:59 +0530

    Initial project upload

---
+++ Backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const authRouter = require("./routes/auth.route");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const interviewRouter = require("./routes/interview.route");
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
+
+module.exports = app;
---
+++ Backend/src/models/blacklist.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const blacklistTokenSchema = new mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: [true, "Token is required to be added to the blacklist"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const tokenBlacklistModel = mongoose.model(
+  "blacklisttokens",
+  blacklistTokenSchema,
+);
+
+module.exports = tokenBlacklistModel;
