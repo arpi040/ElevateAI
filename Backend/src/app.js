fb5e8d479e0d8ea1a91c35e698b13537c9bb8db6
Arpita <arpitaver040@gmail.com>
2026-07-30 13:59:56 +0530

    Fix CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,9 +7,23 @@ const interviewRouter = require("./routes/interview.route");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://elevate-ai-appi2.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      // Allow Postman and requests without an Origin header
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   }),
 );
